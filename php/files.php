feca42df514741a8c639b36e7e5e1e15ef4d568e
Artem <artem.lutenkov@gmail.com>
2018-12-17 00:26:03 +0300

    3 Lab
    processing file, delete even numbers

--- php/files.php
+++ php/files.php
@@ -1,13 +1,33 @@
 <?php
-$n = (int)$_POST['dim']; // разбивает строку на массив символов
-$result = 0;
-for ($i = 0; $i < $n; $i++) {
-    for($j = 0; $j < $n; $j++) {
-        $d = (int)$_POST['arr' . strval($i) . strval($j)];
-        if($d < 0) {
-            $result += 0 - $d;
+$file_path= $_POST['file']; 
+$out_file_path=$_POST['out_file'];
+$fp = fopen($file_path, 'r');
+$fo = fopen($out_file_path, 'r+');
+if ($fp) 
+{
+    $cnt = (int)0;
+    $num = (int)0;
+    while(!feof($fp))
+    {
+        $token = fgetc($fp);
+        if (ord($token) < 48 or ord($token) > 57) {
+            if($cnt != 0) {
+                if($num % 2 != 0)
+                    fwrite($fo, strval($num));
+            }
+            $cnt = (int)0;
+            $num = (int)0;
+            fwrite($fo, strval($token));
+        }
+        else{
+            $num = $num*(int)10 + ord($token) - 48;
+            $cnt++;
         }
     }
+    if($num % 2 != 0)
+        fwrite($fo, strval($num));
 }
-echo $result; // выводим результат
+fclose($fp);
+fclose($fo);
+echo "файл записан!"
 ?>
